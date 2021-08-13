600084f74dfa08409e16a9299de52c472991d720
Sizan-Ahmed <asizan631@gmail.com>
2021-08-13 21:14:09 +0600

    more change

--- player.js
+++ player.js
@@ -4,73 +4,47 @@ const wScoreDisplay = document.querySelector('p span');
 const inputScore = document.getElementById('inputScore');
 const p1Btn = document.getElementById('p1Btn');
 const p2Btn = document.getElementById('p2Btn');
-const resetBtn = document.getElementById('resetBtn');
+let resetBtn = document.getElementById('resetBtn');
 let p1Score = 0;
 let p2Score = 0;
 const winningScore = 5;
 let gameOver = false;
 
 p1Btn.addEventListener("click", () => {
-    // data change
-    p1Score++;
-    if (p1Score === winningScore) {
-        // gameOver
-        p1Btn.setAttribute("disabled", "disabled")
-        p2Btn.setAttribute("disabled", "disabled")
+
+    if (!gameOver) {
+        p1Score++;
+        winner(p1Score, winningScore)
+        p1ScoreDisplay.textContent = p1Score;
     }
-    // show change data
-    p1ScoreDisplay.textContent = p1Score;
+
 
 })
 
 p2Btn.addEventListener("click", () => {
-    // data change
-    p2Score++;
-    if (p2Score === winningScore) {
-        // gameOver
-        p1Btn.setAttribute("disabled", "disabled")
-        p2Btn.setAttribute("disabled", "disabled")
+
+    if (!gameOver) {
+        p2Score++;
+        winner(p2Score, winningScore)
+        p2ScoreDisplay.textContent = p2Score;
     }
-    // show change data
-    p2ScoreDisplay.textContent = p2Score;
 
 })
 
-// pBtn1.addEventListener("click", function() {
-//     if (!gameOver) {
-//         p1Score++;
-//         player1.textContent = p1Score;
-//         winner(p1Score, winningScore)
+resetBtn.addEventListener("click", () => {
+    p1Score = 0;
+    p2Score = 0;
+    gameOver = false;
+    p1ScoreDisplay.textContent = 0;
+    p2ScoreDisplay.textContent = 0;
+    p1Btn.removeAttribute("disabled");
+    p2Btn.removeAttribute("disabled");
+})
 
-//     }
-
-// })
-
-// pBtn2.addEventListener("click", function() {
-//     if (!gameOver) {
-//         p2Score++;
-//         player2.textContent = p2Score;
-//         winner(p2Score, winningScore)
-//     }
-
-// })
-
-// function winner(oldScore, winningScore) {
-//     if (oldScore === winningScore) {
-//         gameOver = true;
-//         console.log("game over");
-//         pBtn1.setAttribute('disabled', 'disabled');
-//         pBtn2.setAttribute('disabled', 'disabled');
-//     }
-// }
-
-// resetBtn.addEventListener("click", function() {
-//     p1Score = 0;
-//     p2Score = 0;
-//     gameOver = false;
-//     player1.textContent = p1Score;
-//     player2.textContent = p2Score;
-//     pBtn1.removeAttribute('disabled');
-//     pBtn2.removeAttribute('disabled');
-
-// })
+function winner(oldScore, winningScore) {
+    if (oldScore === winningScore) {
+        gameOver = true
+        p1Btn.setAttribute("disabled", "disabled")
+        p2Btn.setAttribute("disabled", "disabled")
+    }
+}
